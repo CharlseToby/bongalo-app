4da1bd3d0ab4d9b9548ce766fbde71800c1a3dae
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-06-14 22:42:33 +0100

    carousel fixed

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -6,18 +6,18 @@ import useWindowDimensions from '../../hooks/useWindowsDimensions';
 
 
 function Carousel(props:any) {
+  const {type, cardSize, name} = props;
   const { width: windowWidth = 1443} = useWindowDimensions();
   let slideCount = 4;
-
+  
   if (windowWidth <= 1000 && windowWidth > 600) {
     slideCount = 3;
   } else if (windowWidth <= 600) {
-    slideCount = 2;
+    slideCount = 2; 
   }
-
   
-  const {type, cardSize, name} = props;
-  // console.log(type);
+  let countDivider = (type.length / slideCount) + 1;
+  
   const slideInterval:{current: NodeJS.Timer | null} = useRef(null);
   
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -37,25 +37,25 @@ function Carousel(props:any) {
   const startSlideTimer = () => {
     stopSlideTimer();
     slideInterval.current = setInterval(() => {
-      setCurrentSlide(currentSlide => (currentSlide >= type.length/slideCount - 1)? 0 : currentSlide + 1)
+      setCurrentSlide(currentSlide => (currentSlide >= (type.length + 1 )/slideCount - 1)? 0 : currentSlide + 1)
     }, 3000);
   }
 
 
   const next = () => {
     startSlideTimer();
-    currentSlide < type.length/4 - 1 ? setCurrentSlide(currentSlide => currentSlide + 1) : setCurrentSlide(0);
+    currentSlide < type.length/slideCount - 1 ? setCurrentSlide(currentSlide => currentSlide + 1) : setCurrentSlide(0);
   }
 
   const prev = () => {
     startSlideTimer();
-    currentSlide > 0 ? setCurrentSlide(currentSlide => currentSlide - 1) : setCurrentSlide(type.length/4);
+    currentSlide > 0 ? setCurrentSlide(currentSlide => currentSlide - 1) : setCurrentSlide(type.length/slideCount);
   }
 
 
   return (
     <div className='carousel'>
-      <div className='carousel-container' style={{transform: `translateX(${-currentSlide/4 * 100}%)`}}>
+      <div className='carousel-container' style={{transform: `translateX(${-currentSlide/countDivider * 100}%)`}}>
         <Slide section={type} cardSize={cardSize} name={name} stopSlide={stopSlideTimer}  startSlide={startSlideTimer}/>
       </div>
       <div className='arrowLeft' onClick={prev}>
